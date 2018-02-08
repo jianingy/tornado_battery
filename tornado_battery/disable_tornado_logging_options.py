2b6c096d7b37f69e4e0179190fc733c62ac8a9fa
Jianing Yang <jianingy.yang@gmail.com>
2018-02-08 17:26:29 +0800

    add disable_tornado_logging_options

---
+++ tornado_battery/disable_tornado_logging_options.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+#
+#        H A P P Y    H A C K I N G !
+#              _____               ______
+#     ____====  ]OO|_n_n__][.      |    |
+#    [________]_|__|________)<     |YANG|
+#     oo    oo  'oo OOOO-| oo\\_   ~o~~o~
+# +--+--+--+--+--+--+--+--+--+--+--+--+--+
+#               Jianing Yang @  8 Feb, 2018
+#
+
+from tornado import log as tornado_log
+tornado_log.define_logging_options = lambda x: x
