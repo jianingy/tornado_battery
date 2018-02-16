36df3d070c80ad9d8abf2373f6dfb926e3d8dd15
Jianing Yang <jianingy.yang@gmail.com>
2018-02-16 11:49:01 +0800

    add requirements.txt

--- setup.py
+++ setup.py
@@ -16,9 +16,4 @@ version = '0.2.1'
 setup(name=package,
       version=version,
       description="a set of utilities help write tornado apps quickly and easily",
-      url='https://github.com/jianingy/tornado_battery',
-      install_requires=['tornado>=4.5.3',
-                        'ujson>=1.3.5',
-                        'colorlog>=3.1.2',
-                        'aioredis>=1.0.0',
-                        'momoko>=2.2.4'])
+      url='https://github.com/jianingy/tornado_battery')
