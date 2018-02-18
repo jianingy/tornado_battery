ee25ba0c8488b67d963c0ccdad433e6359bab9ba
Jianing Yang <jianingy.yang@gmail.com>
2018-02-18 11:30:41 +0800

    add message queue module

---
+++ tests/test_queue.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+#
+#        H A P P Y    H A C K I N G !
+#              _____               ______
+#     ____====  ]OO|_n_n__][.      |    |
+#    [________]_|__|________)<     |YANG|
+#     oo    oo  'oo OOOO-| oo\\_   ~o~~o~
+# +--+--+--+--+--+--+--+--+--+--+--+--+--+
+#               Jianing Yang @ 18 Feb, 2018
+#
+from tornado_battery.queue import register_queue_options, QueueConnectorError
+from tornado_battery.queue import JSONQueue, JSONQueueError
+import pytest
+
+pytestmark = pytest.mark.asyncio
+register_queue_options("test", "amqp://guest:guest@127.0.0.1:5672/%2F")
+register_queue_options("vanilla", "amqp://guest:guest@127.0.0.1:5672/%2F")
+
+
+@pytest.fixture
+async def queue():
+    pika = JSONQueue.instance("test")
+    await pika.connect()
+    return pika
+
+
+@pytest.fixture
+async def vanilla_queue():
+    pika = JSONQueue.instance("vanilla")
+    await pika.connect()
+    return pika
+
+
+async def test_publish_consume(queue):
+    await queue.setup(queue="test_queue", exchange="test_exchange")
+    await queue.publish(dict(value=1984))
+
+    data, msg = await queue.consume()
+    msg.ack()
+    assert data == dict(value=1984)
+
+
+async def test_no_setup_publish(vanilla_queue):
+    with pytest.raises(JSONQueueError, match="^no active exchange set$"):
+        await vanilla_queue.publish(dict(value=1984))
+
+
+async def test_no_setup_consume(vanilla_queue):
+    with pytest.raises(JSONQueueError, match="^no active queue set$"):
+        await vanilla_queue.consume()
+
+
+async def test_no_connection():
+    match = r"^no connection found$"
+    with pytest.raises(QueueConnectorError, match=match):
+        JSONQueue.connection("test")
+
+
+async def test_option_name():
+    from tornado_battery.queue import option_name
+
+    assert option_name("master", "uri") == "queue-uri"
+    assert option_name("slave", "uri") == "queue-slave-uri"
---
+++ tornado_battery/queue.py
@@ -0,0 +1,100 @@
+# -*- coding: utf-8 -*-
+#
+#        H A P P Y    H A C K I N G !
+#              _____               ______
+#     ____====  ]OO|_n_n__][.      |    |
+#    [________]_|__|________)<     |YANG|
+#     oo    oo  'oo OOOO-| oo\\_   ~o~~o~
+# +--+--+--+--+--+--+--+--+--+--+--+--+--+
+#               Jianing Yang @ 17 Feb, 2018
+#
+from .exception import ServerException
+from .pattern import NamedSingletonMixin
+from aio_pika import connect_robust, Message
+from tornado.options import define, options
+from ujson import dumps as json_encode, loads as json_decode
+
+import asyncio
+import logging
+
+LOG = logging.getLogger('tornado.application')
+
+
+class QueueConnectorError(ServerException):
+    pass
+
+
+class QueueConnector(NamedSingletonMixin):
+
+    def __init__(self, name):
+        self.name = name
+
+    async def connect(self, event_loop=None):
+        if event_loop is None:
+            event_loop = asyncio.get_event_loop()
+        name = self.name
+        opts = options.group_dict('%s queue' % name)
+        uri = opts[option_name(name, "uri")]
+        LOG.info('connecting amqp [%s] %s' % (self.name, uri))
+        self._connection = await connect_robust(uri, loop=event_loop)
+
+    def connection(self):
+        if not hasattr(self, '_connection') or not self._connection:
+            raise QueueConnectorError("no connection found")
+        return self._connection
+
+
+class JSONQueueError(ServerException):
+    pass
+
+
+class JSONQueue(QueueConnector):
+
+    async def setup(self, queue=None, exchange='', routing_key='',
+                    durable=False):
+        channel = await self.connection().channel()
+        exchange = await channel.declare_exchange(exchange)
+        if queue:
+            queue = await channel.declare_queue(queue, auto_delete=True,
+                                                durable=durable)
+            await queue.bind(exchange, routing_key)
+            self.active_queue = queue
+        self.routing_key = routing_key
+        self.active_exchange = exchange
+
+    async def publish(self, content):
+        if not hasattr(self, "active_exchange"):
+            raise JSONQueueError("no active exchange set")
+
+        message = Message(bytes(json_encode(content), "utf-8"),
+                          content_type="application/json")
+        await self.active_exchange.publish(message, self.routing_key)
+
+    async def consume(self):
+        if not hasattr(self, "active_queue"):
+            raise JSONQueueError("no active queue set")
+
+        msg = await self.active_queue.get()
+        if msg.content_type != 'application/json':
+            msg.reject(requeue=False)
+            return None
+        try:
+            data = json_decode(msg.body)
+        except ValueError as e:
+            msg.reject(requeue=False)
+            return None
+        return data, msg
+
+
+def option_name(instance, option):
+    if instance == 'master':
+        return 'queue-%s' % option
+    else:
+        return 'queue-%s-%s' % (instance, option)
+
+
+def register_queue_options(instance='master', default_uri='amqp:///'):
+    define(option_name(instance, "uri"),
+           default=default_uri,
+           group='%s queue' % instance,
+           help="queue connection uri for %s" % instance)
