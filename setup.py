8b7cea50b7d9a9154c719d15da5de0f46d9b7e9d
M@gicAgCl(DEAD) <wyqsmith@aegeanstudio.com>
2018-03-27 13:23:44 +0800

    Add Dependencies

--- setup.py
+++ setup.py
@@ -13,9 +13,20 @@ from setuptools import setup, find_packages
 package = 'tornado_battery'
 version = '0.5.12'
 
-setup(name=package,
-      version=version,
-      description="a set of utilities help write tornado apps quickly and easily",
-      url='https://github.com/jianingy/tornado_battery',
-      setup_requires=['pytest-runner',],
-      packages=find_packages())
+setup(
+    name=package,
+    version=version,
+    description="a set of utilities help write tornado apps quickly and easily",
+    url='https://github.com/jianingy/tornado_battery',
+    setup_requires=['pytest-runner'],
+    packages=find_packages(),
+    install_requires=[
+        'tornado',
+        'ujson',
+        'colorlog',
+        'aioredis',
+        'aiopg',
+        'aiomysql',
+        'aio-pika',
+    ],
+)
