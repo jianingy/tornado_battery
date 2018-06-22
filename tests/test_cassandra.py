7dbd3d0f38b627a9122e55d157b71ace29cc2826
liujinliu <liujinliu@doudoubianli.com>
2018-06-22 15:40:15 +0800

    加入cassandra

---
+++ tests/test_cassandra.py
@@ -0,0 +1,107 @@
+# -*- coding: utf-8 -*-
+#
+#        H A P P Y    H A C K I N G !
+#              _____               ______
+#     ____====  ]OO|_n_n__][.      |    |
+#    [________]_|__|________)<     |YANG|
+#     oo    oo  'oo OOOO-| oo\\_   ~o~~o~
+# +--+--+--+--+--+--+--+--+--+--+--+--+--+
+#               Jianing Yang @ 16 Feb, 2018
+#
+import time
+from tornado_battery.cassandra import (
+    register_cassandra_options, with_cassandra)
+from tornado_battery.cassandra import (
+    CassandraConnector, CassandraConnectorError)
+import pytest
+
+pytestmark = pytest.mark.asyncio
+register_cassandra_options("test")
+
+
+@pytest.fixture
+async def cassandra():
+    cassandra = CassandraConnector.instance("test")
+    await cassandra.connect()
+    session = cassandra.connection()
+    cql_keyspace_create = """
+        CREATE KEYSPACE IF NOT EXISTS TestTmp WITH
+        replication = {'class': 'SimpleStrategy',
+        'replication_factor' : 1}
+    """
+    q = await session.prepare_future(cql_keyspace_create)
+    await session.execute_future(q)
+    q = await session.prepare_future('USE TestTmp')
+    await session.execute_future(q)
+    cql_tb_create = ('CREATE TABLE tmp_tornado_battery_test ('
+                     'specid text PRIMARY KEY,'
+                     'foo text,'
+                     'bar int'
+                     ')')
+    cql_tb_drop = 'DROP TABLE IF EXISTS tmp_tornado_battery_test'
+    q = await session.prepare_future(cql_tb_drop)
+    await session.execute_future(q)
+    q = await session.prepare_future(cql_tb_create)
+    await session.execute_future(q)
+    return cassandra
+
+
+async def test_cql(cassandra):
+    now = int(time.time())
+    cql = """
+        INSERT INTO tmp_tornado_battery_test (specid, foo, bar)
+        VALUES ('test', 'foo0', ?)
+    """
+    session = cassandra.connection()
+    query = await session.prepare_future(cql)
+    await session.execute_future(query, (now, ))
+    cql = """
+        SELECT foo, bar FROM tmp_tornado_battery_test WHERE specid='test';
+    """
+    query = await session.prepare_future(cql)
+    values = await session.execute_future(query)
+    assert(values[0][0] == 'foo0')
+    assert(values[0][1] == now)
+
+
+async def test_decorator(cassandra):
+    now = int(time.time())
+
+    @with_cassandra(name="test")
+    async def _read(now, cassandra=None):
+        cql = """
+            INSERT INTO tmp_tornado_battery_test (specid, foo, bar)
+            VALUES ('test', 'foo0', ?)
+        """
+        query = await cassandra.prepare_future(cql)
+        await cassandra.execute_future(query, (now, ))
+        cql = """
+            SELECT foo, bar FROM tmp_tornado_battery_test WHERE specid='test';
+        """
+        query = await cassandra.prepare_future(cql)
+        values = await cassandra.execute_future(query)
+        return values
+    values = await _read(now)
+    assert(values[0][0] == 'foo0')
+    assert(values[0][1] == now)
+
+
+async def test_decorator_duplicated(cassandra):
+
+    @with_cassandra(name="test")
+    async def _read(cassandra):
+        cql = """
+            SELECT foo, bar FROM tmp_tornado_battery_test WHERE specid='test';
+        """
+        query = await cassandra.prepare_future(cql)
+        values = await cassandra.execute_future(query)
+        return values
+
+    with pytest.raises(CassandraConnectorError):
+        await _read(cassandra=None)
+
+
+async def test_option_name():
+    from tornado_battery.cassandra import option_name
+
+    assert option_name("test", "points") == "cassandra-test-points"
---
+++ tornado_battery/cassandra.py
@@ -0,0 +1,96 @@
+# -*- coding: utf-8 -*-
+#
+#        H A P P Y    H A C K I N G !
+#              _____               ______
+#     ____====  ]OO|_n_n__][.      |    |
+#    [________]_|__|________)<     |YANG|
+#     oo    oo  'oo OOOO-| oo\\_   ~o~~o~
+# +--+--+--+--+--+--+--+--+--+--+--+--+--+
+#               Jianing Yang @ 12 Feb, 2018
+#
+from .exception import ServerException
+from .pattern import NamedSingletonMixin
+from tornado.options import define, options
+from urllib.parse import urlparse
+
+import asyncio
+import functools
+import logging
+from cassandra.cluster import Cluster
+from aiocassandra import aiosession
+
+
+LOG = logging.getLogger('tornado.application')
+
+
+class CassandraConnectorError(ServerException):
+    pass
+
+
+class CassandraConnector(NamedSingletonMixin):
+
+    def __init__(self, name: str):  # NOQA
+        self.name = name
+
+    def connection(self):
+        if not hasattr(self, '_session') or not self._session:
+            raise CassandraConnectorError(
+                "no session of %s found" % self.name)
+        return self._session
+
+    async def connect(self):
+        name = self.name
+        opts = options.group_dict('%s cassandra' % name)
+        points = opts[option_name(name, 'points')]
+        contact_points = points.split(',')
+        port = opts[option_name(name, 'port')]
+        executor_threads = opts[option_name(name, 'executor-threads')]
+        LOG.info('connecting cassandra [%s] %s' % (self.name, points))
+        cluster = Cluster(contact_points=contact_points,
+                          port=port, executor_threads=executor_threads)
+        self._session = cluster.connect()
+        aiosession(self._session)
+
+
+def option_name(instance: str, option: str) -> str:
+    return 'cassandra-%s-%s' % (instance, option)
+
+
+def register_cassandra_options(instance: str='default',
+                               points: str='127.0.0.1',
+                               port: int=9042,
+                               executor_threads: int=2):
+    define(option_name(instance, "points"),
+           default=points,
+           group='%s cassandra' % instance,
+           help='cassandra contact points for %s' % instance)
+    define(option_name(instance, 'port'),
+           default=port,
+           group='%s cassandra' % instance,
+           help='cassandra port for %s ' % instance)
+    define(option_name(instance, 'executor-threads'),
+           default=executor_threads,
+           group='%s cassandra' % instance,
+           help='num of cassandra executor_threads for %s ' % instance)
+
+
+def with_cassandra(name: str):
+
+    def wrapper(function):
+
+        @functools.wraps(function)
+        async def f(*args, **kwargs):
+            cassandra = CassandraConnector.instance(name).connection()
+            if 'cassandra' in kwargs:
+                raise CassandraConnectorError(
+                    'duplicated argument for cassandra %s' % name)
+            kwargs.update({'cassandra': cassandra})
+            retval = await function(*args, **kwargs)
+            return retval
+        return f
+
+    return wrapper
+
+
+def connect_cassandra(name: str):
+    return CassandraConnector.instance(name).connect
