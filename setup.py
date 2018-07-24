82ed5eddd7b88c357ec0ae5ba2cde02c284084b1
Jianing Yang <jianingy.yang@gmail.com>
2018-07-24 16:41:28 +0800

    Bump version: 0.5.21 → 0.5.22

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ from os.path import dirname, realpath, join as path_join
 from setuptools import setup, find_packages
 
 package = 'tornado_battery'
-version = '0.5.21'
+version = '0.5.22'
 
 
 def valid_requirement(line):
